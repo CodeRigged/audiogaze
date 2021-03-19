884060eb44d3f4214f66f8f4df44a88db7e37f08
CodeRigged <bingobookdeath@yahoo.de>
2021-03-19 20:13:52 +0100

    reverts changes made to CreateTrial.vue in previous commit + adds vars to Trial.vue + adds env-template

--- frontend/src/views/CreateTrial.vue
+++ frontend/src/views/CreateTrial.vue
@@ -1,10 +1,44 @@
 <template>
-  <div></div>
+  <v-card class="pa-2">
+    <v-card-title>
+      <v-text-field v-model="trial" label="Trial name" clearable />
+    </v-card-title>
+    <v-card-text>
+      <create-trial />
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn @click="resetTrial" plain>Cancel</v-btn>
+      <v-btn @click="addTrial">Confirm</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
+import {paths} from '@/utils/Enums';
+import CreateTrial from '@/components/pages/test-page/CreateTrial.vue';
+import {mapActions, mapMutations, mapState} from 'vuex';
+
 export default {
-  name: 'trial',
-  title: 'Trial',
-  path: '/trial',
+  components: {
+    CreateTrial,
+  },
+  name: 'new-test',
+  title: 'New trial',
+  path: paths.addTrial,
+  methods: {
+    ...mapMutations('addTrial', ['updateName']),
+    ...mapActions('addTrial', ['addTrial', 'resetTrial']),
+  },
+  computed: {
+    ...mapState('addTrial', ['name']),
+    trial: {
+      get() {
+        return this.name;
+      },
+      set(name) {
+        this.updateName(name);
+      },
+    },
+  },
 };
 </script>
---
+++ frontend/src/views/Trial.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'trial',
+  title: 'Trial',
+  path: '/trial/:id',
+};
+</script>
