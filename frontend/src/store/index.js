e1cfe6dc7ddc89122d593e89d9815e80f401301d
CodeRigged <bingobookdeath@yahoo.de>
2021-03-19 19:15:20 +0100

    added store and data table

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,36 @@
 /* eslint-disable no-unused-vars */
+import {paths} from '@/utils/Enums';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import modules from './modules/all-modules';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    trials: [],
+    paths,
+  },
+  mutations: {
+    updateState(state, input) {
+      const {key, data} = input;
+      state[key] = data;
+    },
+  },
+  actions: {
+    async loadTrials({commit}) {
+      await Vue.axios
+        .get('/trials')
+        .then((res) => {
+          commit('updateState', {key: 'trials', data: res.data});
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    updateTrials({commit}, data) {
+      commit('updateState', {key: 'trials', data});
+    },
+  },
   modules,
   getters: {},
 });
--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,19 +1,52 @@
 <template>
-  <absolute-center>
-    <v-btn label="New Test" to="/test">New Test</v-btn>
-  </absolute-center>
+  <v-data-table
+    :headers="trialHeaders"
+    :items="trials"
+    single-expand
+    :expanded.sync="expanded"
+    item-key="name"
+    show-expand
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Trials</v-toolbar-title>
+        <v-spacer />
+        <v-btn :to="path">New trial</v-btn>
+      </v-toolbar>
+    </template>
+    <template v-slot:expanded-item="{headers, item}">
+      <td :colspan="headers.length">More info about {{ item.name }}</td>
+    </template>
+  </v-data-table>
 </template>
 <script>
+import {paths} from '@/utils/Enums';
 import {mapState} from 'vuex';
-import AbsoluteCenter from '@/components/other/AbsoluteCenter.vue';
 export default {
   name: 'index',
   title: 'Home',
-  path: '/',
+  path: paths.index,
+  data: () => ({expanded: [], singleExpand: false}),
   description: 'Overview page',
-  components: {AbsoluteCenter},
   computed: {
-    ...mapState({path: (state) => state.paths.newTest}),
+    ...mapState({path: (state) => state.paths.addTrial}),
+    trialHeaders() {
+      return [
+        {
+          text: 'Name',
+          align: 'start',
+          value: 'name',
+        },
+        {text: '', value: 'data-table-expand'},
+      ];
+    },
+    trials() {
+      return this.$store.state.trials;
+    },
+  },
+  created() {
+    this.$store.dispatch('loadTrials');
   },
 };
 </script>
