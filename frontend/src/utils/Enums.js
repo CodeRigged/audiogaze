fdc0ad1430f65d80fcba1f420e99917a72128772
CodeRigged <bingobookdeath@yahoo.de>
2021-03-20 05:58:41 +0100

    adds trial page

--- frontend/src/utils/Enums.js
+++ frontend/src/utils/Enums.js
@@ -2,6 +2,7 @@ const {
   // Paths
   BASE_URL: index,
   VUE_APP_PATH_ADD_TRIAL: addTrial,
+  VUE_APP_PATH_RUN_TRIAL: runTrial,
   // CONFIG
   VUE_APP_API_BASE_URL: apiBaseURL,
 } = process.env;
@@ -9,6 +10,7 @@ const {
 const paths = Object.freeze({
   index,
   addTrial,
+  runTrial,
 });
 
 const CONFIG = Object.freeze({
--- frontend/src/views/Trial.vue
+++ frontend/src/views/Trial.vue
@@ -1,10 +1,45 @@
 <template>
-  <div></div>
+  <v-parallax :height="screenHeight" :src="image">
+    <absolute-center class="title">Test starting in 3...</absolute-center>
+    <div class="bottom-right">
+      <v-icon
+        v-if="!isFullScreen"
+        size="70"
+        color="white"
+        @click="toggleFullScreen"
+      >
+        mdi-fullscreen
+      </v-icon>
+    </div>
+  </v-parallax>
 </template>
 <script>
+import Test from '@/assets/images/background-test.jpg';
+import {paths} from '@/utils/Enums';
+import {mapActions, mapState} from 'vuex';
+import AbsoluteCenter from '@/components/other/AbsoluteCenter.vue';
 export default {
+  components: {AbsoluteCenter},
   name: 'trial',
   title: 'Trial',
-  path: '/trial/:id',
+  path: paths.runTrial,
+  data: () => ({}),
+  computed: {
+    ...mapState(['isFullScreen']),
+    image() {
+      return Test;
+    },
+    screenHeight() {
+      return screen.height;
+    },
+  },
+  methods: {...mapActions(['toggleFullScreen'])},
 };
 </script>
+<style lang="scss" scoped>
+.bottom-right {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+</style>
